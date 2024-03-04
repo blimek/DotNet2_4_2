084493feb86a2b568e9268499b1bdfc88cd27bc7
przemek milke <theblimek@gmail.com>
2024-03-04 11:43:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet2_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            char X = 'X';
+            char Y = 'Y';
+            char Z = 'Z';
+
+            Console.WriteLine($" Twoje zmienne to: {Z}, {Y}, {X}");
+            Console.ReadLine();
+
+        }
+    }
+}
